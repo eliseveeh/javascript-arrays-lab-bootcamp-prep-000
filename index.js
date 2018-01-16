66bf10848512ddc4e5350da371f298805d8840f3
Elizabeth Veeh <elise.veeh@gmail.com>
2018-01-16 09:03:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const app = "I don't do much."
 
 function destructivelyAppendKitten(name){
-   name.push("kitten")
-   return name
+   name.push("kitten");
 }
 
-function destructivelyPrependKitten(name){
-  name.shift("oscar")
-  return name
-}
